0c0c61bc20667242fd8e1f3fac4acd88f0966e85
soohooncho <soohoon.cho@samsungfiresvc.com>
2022-12-08 16:42:22 +0900

    [공통] 리스트, 메시지 유팅 생성

---
+++ src/core/List.ts
@@ -0,0 +1,8 @@
+class List {
+    headerTitle: any = {
+        'Dashboard': '랑아트',
+        'Customers': '고객 정보'
+    }
+}
+
+export default new List();
---
+++ src/core/Message.ts
@@ -0,0 +1,10 @@
+class Message {
+    success = {
+        'S001': '성공'
+    }
+    error = {
+        'E001': '에러 발생'
+    }
+}
+
+export default new Message();
