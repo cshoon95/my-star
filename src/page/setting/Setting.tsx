e7966170f7a5799cf3165636fddbbf7a832e0238
soohooncho <soohoon.cho@samsungfiresvc.com>
2022-12-15 16:24:43 +0900

    [세팅] 작업중

--- src/page/setting/Setting.tsx
+++ src/page/setting/Setting.tsx
@@ -5,8 +5,13 @@ import Title from '../../comp/module/Title';
 const Setting = () => {
     return(
         <InitPage>
-            <Title>타이틀</Title>
-            <BoardTransfer/>
+            <div>
+                <Title>목록 선택</Title>
+                <BoardTransfer/>
+            </div>
+            <div>
+                <Title>화면 모드</Title>
+            </div>
         </InitPage>
     )
 }
