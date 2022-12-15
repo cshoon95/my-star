f4bff917369a7018fa31ccf2892128070e309d68
soohooncho <soohoon.cho@samsungfiresvc.com>
2022-12-15 16:24:59 +0900

    [서버] 작업중

--- src/core/Server.ts
+++ src/core/Server.ts
@@ -2,20 +2,57 @@ import axios from 'axios';
 import ons from './Ons'
 
 type OptionsType = {
-    method: 'get' | 'post';
+    method?: 'get' | 'post' | 'put';
     url: string;
     data?: any
 }
 class Server{
+    private _guid: string = '';
     private initAxios = () => {
         return axios.create({
-            baseURL: "http://localhost:3001/api/",
+            baseURL: "http://127.0.0.1:3001/api",
 			// timeout: 5000,
         });
     }
     
+    public get guid() {
+		if(!this._guid){
+			this._guid = (()=>{
+				return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+					const r = Math.random() * 16 | 0, v = c === 'x' ? r : ((r & 0x3) | 0x8);
+					return v.toString(16);
+				});
+			})();
+		}
+		return this._guid;
+	}
+
     private axios = this.initAxios();
 
+    public get(options: OptionsType, callbackFunc?: Function) {
+        ons.showLoading();
+
+        let response: any;
+
+        this.axios.post(options.url, {
+            params: options.data
+        }).then(function (response) {
+            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶ 서버에서 가져온 정보 시작');
+            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶', response);
+            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶ 서버에서 가져온 정보 끝');
+
+            response = response.data;
+            if (callbackFunc) callbackFunc(response);
+          })
+          .catch(function (error) {
+            response = error;
+          })
+          .then(function () {
+            ons.hideLoading();
+          });  
+
+          return response;
+    }
     public run(options: OptionsType, callbackFunc?: Function) {
         ons.showLoading();
 
@@ -24,9 +61,17 @@ class Server{
         this.axios({
             method: options.method,
             url: options.url,
-            data: options.data
+            data: options.data,
+            headers: {
+                "Cache-Control": "no-cache",
+                "X-DIRECT-CLIENT-ID": this.guid
+            }
         })
         .then((res: any) => { 
+            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶ 서버에서 가져온 정보 시작');
+            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶', res);
+            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶ 서버에서 가져온 정보 끝');
+
             response = res.data;
             if (callbackFunc) callbackFunc(response);
         })
@@ -35,9 +80,6 @@ class Server{
             throw new Error(response);
         })
         .finally(() => {
-            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶ 서버에서 가져온 정보 시작');
-            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶', response);
-            ons.log('▨▨▨▨▨▨▨▨▨▨▨▨▶ 서버에서 가져온 정보 끝');
             ons.hideLoading();
         });
 
--- src/page/attendance/Attendance.tsx
+++ src/page/attendance/Attendance.tsx
@@ -1,6 +1,24 @@
 import InitPage from "../../comp/module/InitPage";
+import { useEffect } from "react";
+import ons from "../../core/Ons";
 
 const Attendance = () => {
+    
+    useEffect(() => {
+        ons.server.get({
+        url: 'environment/info/:id',
+        data: {
+            ID: 1
+        }
+        }, (response: any) => {
+            ons.log(response);
+        })
+    })
+    
+    useEffect(() => {
+        
+    })
+
     return (
         <InitPage>
             <p>sdfds</p>
