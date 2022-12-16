14912e4de9024f2443a0620255bce5305e6b3731
soohooncho <soohoon.cho@samsungfiresvc.com>
2022-12-16 15:28:36 +0900

    [공통] 인덱스와 메인 분리

--- src/index.tsx
+++ src/index.tsx
@@ -1,68 +1,24 @@
-import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/Store';
 import ons from './core/Ons';
-import Alert from './manager/Alert';
-import Loading from './manager/Loading';
-import Header from './comp/header/Header';
-import Drawer from './comp/navigation/Drawer';
-import Dashboard from './page/dashboard/Dashboard';
-import Customers from './page/customers/Customers';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import Attendance from './page/attendance/Attendance';
-import Setting from './page/setting/Setting';
-import Reports from './page/reports/Reports';
-
-// start -- MUI 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-// end -- MUI
+import Main from "./main";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-// const darkTheme = createTheme();
-const mdTheme = createTheme({
-    palette: {
-      mode: 'dark',
-    },
-  });
-
 ons.init(store);
 
-const isShowDrawer = ons.getState('isShownDrawer', 'view');
 
 root.render(
     <Provider store = {store}>
         <RecoilRoot>
-            <ThemeProvider theme={mdTheme}>
-                <React.StrictMode>
-                    <div>
-                        <Alert/>
-                        <Loading/>
-                    </div>
-                    <Router>
-                        <Box sx={{ display: 'flex' }}>
-                            <CssBaseline />
-                            <Header open={isShowDrawer}/>
-                            <Drawer />
-                            <Routes>
-                                <Route path="/" element={ <Dashboard /> } />
-                                <Route path="/customers" element={ <Customers /> } />
-                                <Route path="/attendance" element={ <Attendance /> } />
-                                <Route path="/reports" element={ <Reports /> } />
-                                <Route path="/setting" element={ <Setting /> } />
-                            </Routes>
-                        </Box>
-                    </Router>        
-                </React.StrictMode>
-            </ThemeProvider>
+            <Main/>
         </RecoilRoot>
     </Provider>
 );
---
+++ src/main.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { StoreStateType } from './type/Type';
+import Alert from './manager/Alert';
+import Loading from './manager/Loading';
+import Header from './comp/header/Header';
+import Drawer from './comp/navigation/Drawer';
+import Dashboard from './page/dashboard/Dashboard';
+import Customers from './page/customers/Customers';
+import Attendance from './page/attendance/Attendance';
+import Setting from './page/setting/Setting';
+import Reports from './page/reports/Reports';
+
+// start -- MUI 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import ons from './core/Ons';
+// end -- MUI
+
+const Main = () => {
+    const { viewMode, isShowDrawer } = useSelector((state: StoreStateType) => {
+        return {
+            viewMode: state.view.viewMode,
+            isShowDrawer: state.view.isShowDrawer
+        }
+    });
+    
+    const lightTheme = createTheme();
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    });
+    
+    return (
+        <ThemeProvider theme={viewMode === 'dark' ? darkTheme : lightTheme}>
+            <React.StrictMode>
+                <div>
+                    <Alert/>
+                    <Loading/>
+                </div>
+                <Router>
+                    <Box sx={{ display: 'flex' }}>
+                        <CssBaseline />
+                        <Header open={isShowDrawer}/>
+                        <Drawer />
+                        <Routes>
+                            <Route path="/" element={ <Dashboard /> } />
+                            <Route path="/customers" element={ <Customers /> } />
+                            <Route path="/attendance" element={ <Attendance /> } />
+                            <Route path="/reports" element={ <Reports /> } />
+                            <Route path="/setting" element={ <Setting /> } />
+                        </Routes>
+                    </Box>
+                </Router>        
+            </React.StrictMode>
+        </ThemeProvider>
+    )
+}
+
+export default Main;
