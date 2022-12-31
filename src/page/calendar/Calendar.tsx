e61bdbb6a7eda8540fc792cd3ab697ce009bdf22
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-12-31 23:15:51 +0900

    [캘린더] 작업중

--- src/page/calendar/Calendar.tsx
+++ src/page/calendar/Calendar.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 import InitPage from "../../comp/module/InitPage";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import { myevents, myresources } from "./data";
+import { format, parse, startOfWeek, getDay } from "date-fns";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+// import "./myCalendar.css";
 
-const Calnedar = () => {
+const locales = {
+	"en-KR": require("date-fns")
+};
+const localizer = dateFnsLocalizer({
+	format,
+	parse,
+	startOfWeek,
+	getDay,
+	locales
+});
+const MyCalnedar = () => {
     return (
         <InitPage>
-            <p>sd</p>
+            <Calendar
+                events={myevents}
+                localizer={localizer}
+                defaultDate={new Date(2021, 5, 8)}
+                style={{ height: 700 }}
+            />
         </InitPage>
     )
 }
 
-export default Calnedar
+export default MyCalnedar
