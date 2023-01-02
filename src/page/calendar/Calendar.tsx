85e0aa088dc5455a72693733cbfba9c06dfd4726
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2023-01-02 21:31:33 +0900

    [캘린더] 생일자 이벤트 추가

--- src/page/calendar/Calendar.tsx
+++ src/page/calendar/Calendar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import InitPage from "../../comp/module/InitPage";
+import ons from "../../core/Ons";
+
+// start -- MUI 
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 import { myevents, myresources } from "./data";
 import { format, parse, startOfWeek, getDay } from "date-fns";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-// import "./myCalendar.css";
+// end -- MUI
 
 const locales = {
 	"en-KR": require("date-fns")
@@ -17,13 +20,36 @@ const localizer = dateFnsLocalizer({
 	locales
 });
 const MyCalnedar = () => {
+    const sysdate = ons.utils.sysdate(new Date());
+    const today = {
+        yyyy: Number(sysdate.substring(0, 4)),
+        mm: Number(sysdate.substring(4, 6)) - 1,
+        dd: Number(sysdate.substring(6, 8))
+    }
+    
+    const customers = ons.getState('customers');
+    const data = customers.map((item: any) => {
+        return {
+            id: `${item.id}`,
+            title: `${item.NAME} 생일 🎂`,
+            yyyy: `${Number(item.BIRTH.substring(0, 4))}`,
+            mm: `${Number(item.BIRTH.substring(5, 7))}`,
+            dd: `${Number(item.BIRTH.substring(8, 10))}`,
+            start: new Date(Number(item.BIRTH.substring(0, 4)), Number(item.BIRTH.substring(5, 7)) -1, Number(item.BIRTH.substring(8, 10))),
+            end: new Date(Number(item.BIRTH.substring(0, 4)), Number(item.BIRTH.substring(5, 7)) -1, Number(item.BIRTH.substring(8, 10)))
+        }
+    })
+
     return (
         <InitPage>
-            <Calendar
-                events={myevents}
+            <Calendar 
+                events={data}
                 localizer={localizer}
-                defaultDate={new Date(2021, 5, 8)}
-                style={{ height: 700 }}
+                defaultDate={new Date(today.yyyy, today.mm, today.dd)}
+                style={{ height: 750, marginTop: 3 }}
+                onView={(e) => {
+                    debugger
+                }}
             />
         </InitPage>
     )
